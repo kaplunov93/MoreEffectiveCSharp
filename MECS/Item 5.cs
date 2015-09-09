e368ecbdc91f5071be5d11ee118fc2fe9202117d
Alex Kaplunov <kaplunov93@gmail.com>
2015-09-09 16:14:55 +0300

    Chapter 1  Item 5

---
+++ MECS/Item 5.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MECS.Chapter1.Item5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.ReadKey();
+        }
+
+        interface IEngine
+        {
+            void DoWork();
+        }
+
+        //Better to use dispose out from generic class
+        class EngineDriver<T> where T :IEngine
+        {
+            private T driver;
+            public EngineDriver(T driver)
+            {
+                this.driver = driver;
+            }
+            public void GetThingsDone()
+            {
+                driver.DoWork();
+            }
+        }
+
+        sealed class BadEngineDriver<T> : IDisposable where T : IEngine, new()
+        {
+            private T driver;
+
+            public void GetThingsDone()
+            {
+                if (driver == null)
+                    driver = new T();
+                driver.DoWork();
+            }
+
+            public void Dispose()
+            {
+                IDisposable resource = driver as IDisposable;
+                if (resource != null)
+                {
+                    resource.Dispose();
+                }
+            }
+        }
+
+    }
+}

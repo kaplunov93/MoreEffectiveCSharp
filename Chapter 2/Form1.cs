0c568ae82308e2b5893d6ff6f1287faa44ed11f6
Alex Kaplunov <akaplunov@infosoftplus.com>
2015-09-17 16:11:10 +0300

    Chapter 2 Item 16

---
+++ Chapter 2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MECS.Chapter2.Item16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Tick += OnTick;
+            timer1.Start();
+        }
+
+        private void OnTick(object sender, EventArgs e)
+        {
+            Action action = () =>
+            {
+                label1.Text = DateTime.Now.ToLongTimeString();
+                Console.WriteLine(DateTime.Now.ToLongTimeString()); 
+            };
+            if (this.InvokeRequired)
+                this.Invoke(action);
+            else
+                action();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Chapter 2/Item 16.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MECS.Chapter2.Item16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}

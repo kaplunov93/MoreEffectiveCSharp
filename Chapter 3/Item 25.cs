f55a2340cdb0427dd7474ba7b9736e23949a3705
Alex Kaplunov <akaplunov@infosoftplus.com>
2015-09-23 11:03:58 +0300

    Chapter 3 Item 25

---
+++ Chapter 3/Item 25.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MECS.Chapter3.Item25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker my = new Worker();
+            my.TryDoWork(); //Good
+            my.DoWork();    //Worth
+
+            Console.ReadKey();
+        }
+
+        class Worker
+        {
+            public bool TryDoWork()
+            {
+                if (!TestCondition())
+                    return false;
+                Work(); // maybe throw exception
+                return true;
+            }
+
+            private bool TestCondition()
+            {
+                //test parameters & etc...
+                return true;
+            }
+
+            public void DoWork()
+            {
+                Work(); // will throw exception
+            }
+            private void Work()
+            {
+                Thread.Sleep(100);
+            }
+
+        }
+
+    }
+}

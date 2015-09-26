224c66303f18ef1e92a7a156547b8b228450bb7e
Alex Kaplunov <akaplunov@infosoftplus.com>
2015-09-26 13:24:14 +0300

    Chapter 5 Item 37

---
+++ Chapter 5/Item 37.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MECS.Chapter5.Item37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allNums = AllNumbers();
+            //Good
+            var smallNums = allNums.Take(10);
+
+            foreach (var i in smallNums)
+                Console.Write("{0} ",i);
+            Console.WriteLine("\n");
+
+            //Bad
+            smallNums = from n in allNums where n < 10 select n;
+
+            foreach (var i in smallNums)
+                Console.Write("{0} ", i);
+            Console.WriteLine("\n");
+
+            Console.ReadKey();
+        }
+
+        static IEnumerable<int> AllNumbers()
+        {
+            for (int i = 0; i <= int.MaxValue; i++)
+                yield return i;
+        }
+    }
+}

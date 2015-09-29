31945ef41cac959cc4a11e5ecf0abdaa7be6e297
Alex Kaplunov <akaplunov@infosoftplus.com>
2015-09-29 16:46:08 +0300

    Chapter 6 Item 45

---
+++ Chapter 6/Item 45.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MECS.Chapter6.Item45
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            int? j= default(int?);
+            int i = default(int);
+
+            try
+            {
+                i = (int) j;
+            }
+            catch 
+            {
+                Console.WriteLine("Oops Can't convert int? to int ");
+            }
+
+            try
+            {
+                string s = ((object) j).ToString();
+                Console.WriteLine(s);
+            }
+            catch(NullReferenceException)
+            {
+                Console.WriteLine("Oops");
+            }
+
+
+            Console.ReadKey();
+        }                
+    }
+}

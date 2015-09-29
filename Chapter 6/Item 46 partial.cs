17cc08f266fefa2f6ace06d24e3b44150ddf344e
Alex Kaplunov <akaplunov@infosoftplus.com>
2015-09-29 18:56:04 +0300

    Chapter 6 Item 46

---
+++ Chapter 6/Item 46 partial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MECS.Chapter6.Item46
+{
+    public partial class Person
+    {
+        public void Change(string first,string last)
+        {
+            FirstName = first;
+            LastName = last;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}",FirstName,LastName);
+        }
+    }
+}
---
+++ Chapter 6/Item 46.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MECS.Chapter6.Item46
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Person my = new Person();
+            Console.WriteLine(my.ToString());
+            my.Change("Vasia","Pupkin");
+            Console.WriteLine(my.ToString());
+
+            Console.ReadKey();
+        }
+    }
+
+    public partial class Person
+    {
+        public Person(string first="Alex",string last="Kaplunov")
+        {
+            FirstName = first;
+            LastName = last;
+        }
+
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+
+    }
+}

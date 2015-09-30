c10fe4424bf436ed0f38b37a86be8d5f39020432
Alex Kaplunov <akaplunov@infosoftplus.com>
2015-09-30 16:10:30 +0300

    Chapter 6 Item 50

---
+++ Chapter 6/Item 50.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Timers;
+
+namespace MECS.Chapter6.Item50
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ImutablePerson p1 = new ImutablePerson("Alex", "");
+            }
+            catch(ArgumentException e)
+            {
+                Console.WriteLine("{0} in {1}",e.Message,e.TargetSite.DeclaringType.ToString().Split('+').Last());
+            }
+
+            try
+            {
+                MutablePerson p1 = new MutablePerson("", "");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("{0} in {1}", e.Message, e.TargetSite.DeclaringType.ToString().Split('+').Last());
+            }
+
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Imutable PErson class with Validation and explicit fields
+        /// </summary>
+        class ImutablePerson
+        {
+            public ImutablePerson(string first,string last)
+            {
+                FirstName = first;
+                LastName = last;
+            }
+
+            private string _FirstName;
+            private string _LastName;
+            public string FirstName
+            {
+                get { return _FirstName; }
+                private set
+                {
+                    if (string.IsNullOrEmpty(value))
+                        throw new ArgumentException("First name can't be null or empty");
+                    _FirstName = value;
+                }
+            }
+
+            public string LastName
+            {
+                get { return _LastName; }
+                private set
+                {
+                    if (string.IsNullOrEmpty(value))
+                        throw new ArgumentException("Last name can't be null or empty");
+                    _LastName = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Mutable Person Class with out validation on implicit fields
+        /// </summary>
+        class MutablePerson
+        {
+            public MutablePerson(string first="", string last="")
+            {
+                FirstName = first;
+                LastName = last;
+            }
+
+            
+            public string FirstName
+            {
+                get;
+                set;
+            }
+
+            public string LastName
+            {
+                get;
+                set;
+            }
+
+        }
+   }
+
+}

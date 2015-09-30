89977e207f42c68c67171a259d36d614d9025d48
Alex Kaplunov <akaplunov@infosoftplus.com>
2015-09-30 15:23:25 +0300

    Chapter 6 Item 49

---
+++ Chapter 6/Item 49.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Timers;
+
+namespace MECS.Chapter6.Item49
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime now = DateTime.Now;
+            LargeMatrix m = new LargeMatrix();
+            WeakReference w = new WeakReference(m);
+            m = null;
+
+            DateTime now1 = DateTime.Now;
+            Console.WriteLine((now1-now).TotalSeconds);
+            
+            now = DateTime.Now;
+            m = w.Target as LargeMatrix;
+            if (m == null)
+                m = new LargeMatrix();
+            now1 = DateTime.Now;
+            Console.WriteLine((now1 - now).TotalSeconds);
+            
+            Console.ReadKey();
+        }
+
+        class LargeMatrix
+        {
+            public int[,] matrix;
+
+            public LargeMatrix()
+            {
+                matrix = new int[1000, 1000];
+            }
+        }
+        
+    }
+
+}
